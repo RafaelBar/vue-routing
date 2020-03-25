85582b015fc38bbe44af49962f62929cbdf330be
Rafael Bar <rafaelbar37>
2020-03-25 15:53:54 +0200

    basic routing behaviors

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -1,3 +1,10 @@
 <template>
-    <h3>Some User Details</h3>
+    <div class="d">
+        <h3>Some User Details</h3>
+        <p class="">User Loaded has ID: {{$route.params.id}}</p>
+        <router-link 
+            tag="button" 
+            :to="{ name: 'userEdit', params: {id: $route.params.id}, query:{locale:'en', q:'100'} }"
+            class="btn btn-primary">Edit The User</router-link>
+    </div>
 </template>
--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -1,3 +1,7 @@
 <template>
-    <h3>Edit the User</h3>
+    <div class="">
+        <h3>Edit the User</h3>
+        <p class="">Locale: {{$route.query.locale}}</p>
+        <p class="">q: {{$route.query.q}}</p>
+    </div>
 </template>
